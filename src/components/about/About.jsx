0faf3ef62e28dfacdc177e6d787d88f19372da9f
Krishna Borse <125799973+KLI888@users.noreply.github.com>
2024-04-23 16:18:54 +0530

    Update About.jsx

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import './About.css'
 import AboutImg from '../../assets/about.jpg'
-import CV from '../../assets/John-Cv.pdf'
+import CV from '../../assets/Kriss-CV.pdf'
 import Info from './Info'
 
 function About() {
